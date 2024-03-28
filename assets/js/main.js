0983086da6f6628b57da7f4381a18ec612c0c72e
Maxime <maxime.b2494@gmail.com>
2024-03-28 11:19:48 +0100

    scroll bar up sections  finish

--- assets/js/main.js
+++ assets/js/main.js
@@ -116,6 +116,11 @@ let swiperTestimonial = new Swiper('.testimonial__swiper', {
     slidesPerView: 'auto',
     centeredSlides: 'auto',
 
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+
     breakpoints: {
         1150: {
             slidesPerView: 3,
@@ -125,10 +130,33 @@ let swiperTestimonial = new Swiper('.testimonial__swiper', {
   });
 
 /*=============== SHOW SCROLL UP ===============*/ 
-
+const scrollUp = () =>{
+    const scrollUp = document.querySelector('#scroll-up')
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
 
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== DARK LIGHT THEME ===============*/ 
 
